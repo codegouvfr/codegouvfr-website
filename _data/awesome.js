e684c2d3b20de5ff7cdba0b307bb3c6bbe02746d
Bastien Guerry <bzg@gnu.org>
2024-11-08 13:51:56 +0100

    _data/awesome.js: Update upstream URL for data

--- _data/awesome.js
+++ _data/awesome.js
@@ -1,7 +1,7 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-    const URL = "https://code.gouv.fr/data/awesome-codegouvfr.json";
+    const URL = "https://code.gouv.fr/data/awesome.json";
 
     let awesome = await EleventyFetch(URL, {
         duration: "2w",
