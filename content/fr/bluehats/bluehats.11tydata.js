e09c60dce409c6144424deda6961e060076faa6c
Bastien Guerry <bzg@gnu.org>
2025-04-30 07:50:41 +0200

    Fix typos

--- content/fr/bluehats/bluehats.11tydata.js
+++ content/fr/bluehats/bluehats.11tydata.js
@@ -23,23 +23,23 @@ module.exports = {
     ],
     accordionItems: [
         {
-            title: "Vous souhaitez entrer en contact avec les BlueHats ?",
+            title: "Vous souhaitez entrer en contact avec les BlueHats ?",
             content: `
 <p>Utilisez l'un des <a href="/fr/contact/espaces-communication-bluehats/" target="_blank">canaux de communication</a>.</p>`
         },
         {
-            title: "Vous souhaitez participer à la gazette BlueHats ?",
+            title: "Vous souhaitez participer à la gazette BlueHats ?",
             content: `
-<p>N'hésitez pas à <a href="https://code.gouv.fr/newsletters/subscribe/bluehats@mail.codegouv.fr">vous inscrire</a> à la gazette BlueHats et à proposer des informations en nous <a href="mailto:floss@numerique.gouv.fr">envoyant un courriel</a>.</p>`
+<p>N'hésitez pas à <a href="https://code.gouv.fr/infolettre/">vous inscrire</a> à la gazette BlueHats et à proposer des informations en nous <a href="mailto:floss@numerique.gouv.fr">envoyant un courriel</a>.</p>`
         },
         {
-            title: "Vous souhaitez participer à un atelier BlueHats ?",
+            title: "Vous souhaitez participer à un atelier BlueHats ?",
             content: `
 <p>Tout le monde peut y accéder en ligne via <a href="https://webinaire.numerique.gouv.fr/meeting/signin/invite/362/creator/369/hash/14eb55bd230aa1a8b8a98e0ee35b056d0196afcf" target="_blank">ce lien</a>.  Ils sont aussi visibles en rediffusion sur <a href="https://tube.numerique.gouv.fr/a/logicielslibres/video-channels" target="_blank">la chaîne BlueHats de la mission logiciels libres</a>.</p>
-<p>Vous souhaitez proposer un atelier BlueHats 🧢 ?  <a href="mailto:floss@numerique.gouv.fr">Contactez-nous !</a></p>`
+<p>Vous souhaitez proposer un atelier BlueHats 🧢 ?  <a href="mailto:floss@numerique.gouv.fr">Contactez-nous !</a></p>`
         },
 {
-            title: "Vous souhaitez organiser un atelier ou événement BlueHats ?",
+            title: "Vous souhaitez organiser un atelier ou événement BlueHats ?",
             content: `
 <p>Tout agent public peut solliciter son administration pour organiser un événement BlueHats, en ligne ou en présentiel.  Si vous avez besoin de visuels pour communiquer, vous pouvez utiliser ce <a href="https://github.com/bluehats/bluehats-visuals/archive/refs/tags/v0.7.zip">kit BlueHats</a>.</p>`
         }
